4eafb550932de8c5bf059ec3fdd9946cc03af3ec
javierjararenteria@gmail.com <202160023@ucc.mx>
2024-02-20 15:31:52 -0600

    test remove and edit

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,79 +10,80 @@ import { ModalFormComponent } from '../modal-form/modal-form.component';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  users: User[] = [
-    {
-      gender: 'male',
-      name: { title: 'Mr', first: 'Gustavo', last: 'González' },
-      email: 'gustavo.gonzalez@example.com',
-      phone: '937-749-666',
-      picture: {
-        large: 'https://i.imgur.com/3PmoWvC.jpg',
-        medium: 'https://i.imgur.com/3PmoWvC.jpg',
-        thumbnail: 'https://i.imgur.com/3PmoWvC.jpg',
-      },
-    },
-    {
-      gender: 'female',
-      name: { title: 'Ms', first: 'Emma', last: 'Peltonen' },
-      email: 'emma.peltonen@example.com',
-      phone: '07-781-882',
-      picture: {
-        large: 'https://i.imgur.com/3PmoWvC.jpg',
-        medium: 'https://i.imgur.com/3PmoWvC.jpg',
-        thumbnail: 'https://i.imgur.com/3PmoWvC.jpg',
-      },
-    },
-    {
-      gender: 'female',
-      name: { title: 'Ms', first: 'Sofie', last: 'Christiansen' },
-      email: 'sofie.christiansen@example.com',
-      phone: '80641912',
-      picture: {
-        large: 'https://i.imgur.com/3PmoWvC.jpg',
-        medium: 'https://i.imgur.com/3PmoWvC.jpg',
-        thumbnail: 'https://i.imgur.com/3PmoWvC.jpg',
-      },
-    },
-    {
-      gender: 'male',
-      name: { title: 'Mr', first: 'Konsta', last: 'Jarvela' },
-      email: 'konsta.jarvela@example.com',
-      phone: '04-066-144',
-      picture: {
-        large: 'https://i.imgur.com/3PmoWvC.jpg',
-        medium: 'https://i.imgur.com/3PmoWvC.jpg',
-        thumbnail: 'https://i.imgur.com/3PmoWvC.jpg',
-      },
-    },
-    {
-      gender: 'female',
-      name: { title: 'Ms', first: 'Ljiljana', last: 'Mančić' },
-      email: 'ljiljana.mancic@example.com',
-      phone: '019-2746-930',
-      picture: {
-        large: 'https://i.imgur.com/3PmoWvC.jpg',
-        medium: 'https://i.imgur.com/3PmoWvC.jpg',
-        thumbnail: 'https://i.imgur.com/3PmoWvC.jpg',
-      },
-    },
-    {
-      gender: 'female',
-      name: { title: 'Ms', first: 'Milla', last: 'Mattila' },
-      email: 'milla.mattila@example.com',
-      phone: '09-803-574',
-      picture: {
-        large: 'https://i.imgur.com/3PmoWvC.jpg',
-        medium: 'https://i.imgur.com/3PmoWvC.jpg',
-        thumbnail: 'https://i.imgur.com/3PmoWvC.jpg',
-      },
-    },
-  ];
-  // constructor(private userApiService: UserApiService) {
-  //   this.userApiService.getUsers().subscribe((data) => {
-  //     this.users = data.results;
-  //   });
-  // }
+  // users: User[] = [
+  //   {
+  //     gender: 'male',
+  //     name: { title: 'Mr', first: 'Gustavo', last: 'González' },
+  //     email: 'gustavo.gonzalez@example.com',
+  //     phone: '937-749-666',
+  //     picture: {
+  //       large: 'https://i.imgur.com/3PmoWvC.jpg',
+  //       medium: 'https://i.imgur.com/3PmoWvC.jpg',
+  //       thumbnail: 'https://i.imgur.com/3PmoWvC.jpg',
+  //     },
+  //   },
+  //   {
+  //     gender: 'female',
+  //     name: { title: 'Ms', first: 'Emma', last: 'Peltonen' },
+  //     email: 'emma.peltonen@example.com',
+  //     phone: '07-781-882',
+  //     picture: {
+  //       large: 'https://i.imgur.com/3PmoWvC.jpg',
+  //       medium: 'https://i.imgur.com/3PmoWvC.jpg',
+  //       thumbnail: 'https://i.imgur.com/3PmoWvC.jpg',
+  //     },
+  //   },
+  //   {
+  //     gender: 'female',
+  //     name: { title: 'Ms', first: 'Sofie', last: 'Christiansen' },
+  //     email: 'sofie.christiansen@example.com',
+  //     phone: '80641912',
+  //     picture: {
+  //       large: 'https://i.imgur.com/3PmoWvC.jpg',
+  //       medium: 'https://i.imgur.com/3PmoWvC.jpg',
+  //       thumbnail: 'https://i.imgur.com/3PmoWvC.jpg',
+  //     },
+  //   },
+  //   {
+  //     gender: 'male',
+  //     name: { title: 'Mr', first: 'Konsta', last: 'Jarvela' },
+  //     email: 'konsta.jarvela@example.com',
+  //     phone: '04-066-144',
+  //     picture: {
+  //       large: 'https://i.imgur.com/3PmoWvC.jpg',
+  //       medium: 'https://i.imgur.com/3PmoWvC.jpg',
+  //       thumbnail: 'https://i.imgur.com/3PmoWvC.jpg',
+  //     },
+  //   },
+  //   {
+  //     gender: 'female',
+  //     name: { title: 'Ms', first: 'Ljiljana', last: 'Mančić' },
+  //     email: 'ljiljana.mancic@example.com',
+  //     phone: '019-2746-930',
+  //     picture: {
+  //       large: 'https://i.imgur.com/3PmoWvC.jpg',
+  //       medium: 'https://i.imgur.com/3PmoWvC.jpg',
+  //       thumbnail: 'https://i.imgur.com/3PmoWvC.jpg',
+  //     },
+  //   },
+  //   {
+  //     gender: 'female',
+  //     name: { title: 'Ms', first: 'Milla', last: 'Mattila' },
+  //     email: 'milla.mattila@example.com',
+  //     phone: '09-803-574',
+  //     picture: {
+  //       large: 'https://i.imgur.com/3PmoWvC.jpg',
+  //       medium: 'https://i.imgur.com/3PmoWvC.jpg',
+  //       thumbnail: 'https://i.imgur.com/3PmoWvC.jpg',
+  //     },
+  //   },
+  // ];
+  users: User[] = [];
+  constructor(private userApiService: UserApiService) {
+    this.userApiService.getUsers().subscribe((data) => {
+      this.users = data.results;
+    });
+  }
 
   isLogedIn = false;
   isModalVisible = false;
